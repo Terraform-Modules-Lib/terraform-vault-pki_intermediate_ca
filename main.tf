839a2964727eb96f415f27d73ee618438c6435f7
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-03-21 16:53:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,6 +60,6 @@ resource "vault_pki_secret_backend_config_urls" "this" {
   
   backend = vault_pki_secret_backend_intermediate_set_signed.this.backend
   
-  issuing_certificates = ["${each.value}/v1/${vault_mount.this.path}/ca"]
-  crl_distribution_points = ["${each.value}/v1/${vault_mount.this.path}/crl"]
+  issuing_certificates = [ for url in local.urls_prefix: "${url}/v1/${vault_mount.this.path}/ca" ]
+  crl_distribution_points = [ for url in local.urls_prefix: "${url}/v1/${vault_mount.this.path}/crl" ]
 }
